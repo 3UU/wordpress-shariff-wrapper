556fc849a8b24ca29aa2eb42bf29c8dfce577211
startef <not@posteo.de>
2015-04-15 00:45:56 +0200

    update-js-Heise-193-mail

---
+++ src/js/services/mail.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = function(shariff) {
+    var url = encodeURIComponent(shariff.getURL());
+    
+    var title = shariff.getMeta('DC.title');
+    var creator = shariff.getMeta('DC.creator');
+    
+    if (title.length > 0 && creator.length > 0) {
+        title += ' - ' + creator;
+    } 
+    else {
+        title = shariff.getTitle();
+    }
+    
+    return {
+        popup: false,
+        mobileonly: false,
+        shareText: 'mail',
+        name: 'mail',
+        faName: 's3uu-envelope',
+        title: {
+            'de': 'Per E-Mail versenden',
+            'en': 'Send by email',
+            'es': 'Enviar por email',
+            'fr': 'Envoyer par courriel',
+            'it': 'Inviare via email',
+            'da': 'Sende via e-mail',
+            'nl': 'Sturen via e-mail '
+        },
+        // shareUrl: shariff.getOption('mailUrl')
+        // for future changes - currently the same as the mailto.js
+        shareUrl: 'mailto:?body=' + url + shariff.getReferrerTrack() + '&subject=' + encodeURIComponent(title)
+    };
+    };
+};
