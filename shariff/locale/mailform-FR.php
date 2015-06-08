532b9f95dc93ab67d96e2969f6bd39b88ff7fa8e
startef <not@posteo.de>
2015-06-08 21:51:12 +0200

    mailform-translations

---
+++ shariff/locale/mailform-FR.php
@@ -0,0 +1,36 @@
+<?php
+// strings for the e-mail form in French
+
+// mail form
+$mf_headline['FR'] 	 = 'Diesen Beitrag per E-Mail versenden';
+$mf_headinfo['FR'] 	 = 'Sie können maximal fünf Empf&auml;nger angeben. Diese bitte durch Kommas trennen.';
+$mf_rcpt['FR']		 = 'Empf&auml;nger E-Mail-Adresse(n)';
+$mf_rcpt_ph['FR']	 = 'empf&auml;nger@domain.de';
+$mf_from['FR']		 = 'Absender E-Mail-Adresse';
+$mf_from_ph['FR']	 = 'absender@domain.de';
+$mf_name['FR']		 = 'Name des Absenders (optional)';
+$mf_name_ph['FR']	 = 'Ihr Name';
+$mf_comment['FR']	 = 'Zusatztext (optional)';
+$mf_send['FR']		 = 'E-Mail senden';
+$mf_info['FR']		 = 'Die hier eingegebenen Daten werden nur dazu verwendet, die E-Mail in Ihrem Namen zu versenden. Sie werden nicht gespeichert und es erfolgt keine Weitergabe an Dritte oder eine Analyse zu Marketing-Zwecken.';
+$mf_optional['FR']	 = ' (optional)';
+$mf_wait1['FR']		 = 'Spam-Schutz: Bitte in ';
+$mf_wait2['FR']		 = ' Sekunden noch einmal versuchen!';
+$mf_to_error['FR']	 = 'Ungültige Empf&auml;nger-Adresse(n)!';
+$mf_from_error['FR'] = 'Ungültige Absender-Adresse!';
+
+// send confirmation
+$mf_mail_send['FR']  = 'Die E-Mail wurde erfolgreich gesendet an:';
+
+// e-mail content
+$mf_mailbody1['FR']  = 'Der folgende Beitrag wurde Ihnen von ';
+$mf_mailbody2['FR']  = 'jemanden'; // will be replaced with a name, if provided
+$mf_mailbody3['FR']  = ' empfohlen:';
+
+// e-mail footer
+$mf_footer['FR']     = 'Diese E-Mail wurde ueber das WordPress Plugin \"Shariff Wrapper\" versendet.\r\n';
+$mf_footer['FR']    .= 'Es wurde entwickelt, um die Privatsphaere der Webseitenbesucher zu schuetzen.\r\n';
+$mf_footer['FR']    .= 'Der Seitenbetreiber hat daher keine Moeglichkeit, naehere Angaben zum\r\n';
+$mf_footer['FR']    .= 'tatsaechlichen Absender dieser E-Mail zu geben.\r\n';
+
+?>
