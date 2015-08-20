bcfa837360cf1375b5c103f83f446010e925fba5
startef <not@posteo.de>
2015-08-20 23:56:37 +0200

    fix-backend-on-multisite

--- shariff/backend/index.php
+++ shariff/backend/index.php
@@ -11,26 +11,25 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Heise\Shariff\Backend;
 use Zend\Config\Reader\Json;
 
-class Application {
-  public static function run() {
-	header('Content-type: application/json');
-	// exit if no url is provided
-	if ( ! isset( $_GET["url"] ) ) { 
-		echo 'No URL provided!';
-		return; 
-	}
+header('Content-type: application/json');
 
-	// if we have a json config file
-	if ( is_readable( dirname( __FILE__ ) . '/shariff.json' ) ) {
-	  $reader = new \Zend\Config\Reader\Json();
-	  $tmp = $reader->fromFile( dirname( __FILE__ ) . '/shariff.json' ); 
-	}
+// exit if no url is provided
+if ( ! isset( $_GET["url"] ) ) { 
+	echo 'No URL provided!';
+	return; 
+}
+
+// if we have a json config file
+if ( is_readable( dirname( __FILE__ ) . '/shariff.json' ) ) {
+  $reader = new \Zend\Config\Reader\Json();
+  $tmp = $reader->fromFile( dirname( __FILE__ ) . '/shariff.json' ); 
+}
 	   
-	// check, if user has changed it to his domain
-	if ( ( $tmp['domain'] == 'www.example.com' ) || ( $tmp['domain'] == 'www.heise.de' ) || empty( $tmp['domain'] ) ) $tmp['domain'] = $_SERVER['HTTP_HOST'];
+// check, if user has changed it to his domain
+if ( ( $tmp['domain'] == 'www.example.com' ) || ( $tmp['domain'] == 'www.heise.de' ) || empty( $tmp['domain'] ) ) $tmp['domain'] = $_SERVER['HTTP_HOST'];
 	
-	// check mandatory services array
-	if ( ! is_array( $tmp["services"] ) ) $tmp["services"] = array("0"=>"GooglePlus", 
+// check mandatory services array
+if ( ! is_array( $tmp["services"] ) ) $tmp["services"] = array("0"=>"GooglePlus", 
 														"1"=>"Twitter", 
 														"2"=>"Facebook",
 														"3"=>"LinkedIn",
@@ -41,86 +40,83 @@ class Application {
 														"8"=>"Xing",
 														"9"=>"Tumblr");
 	
-	// force a short init because we only need WP core
-	define( 'SHORTINIT', true );
+// force a short init because we only need WP core
+define( 'SHORTINIT', true );
 	
-	// build the wp-load.php path
-	$wp_root_path = dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) );
+// build the wp-load.php path
+$wp_root_path = dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) );
 	
-	// include ms-functions.php for MS
-	require ( $wp_root_path . '/wp-includes/ms-functions.php' );
+// include ms-functions.php for MS
+require ( $wp_root_path . '/wp-includes/ms-functions.php' );
 	
-	// include formatting.php for untrailingslashit()
-	require ( $wp_root_path . '/wp-includes/formatting.php' );
+// include formatting.php for untrailingslashit()
+require ( $wp_root_path . '/wp-includes/formatting.php' );
 	
-	// include wp-load.php file (that loads wp-config.php and bootstraps WP)
-	require ( $wp_root_path . '/wp-load.php' );
+// include wp-load.php file (that loads wp-config.php and bootstraps WP)
+require ( $wp_root_path . '/wp-load.php' );
 	
-    // include link-template.php for site_url()
-	require ( $wp_root_path . '/wp-includes/link-template.php' );
+// include link-template.php for site_url()
+require ( $wp_root_path . '/wp-includes/link-template.php' );
 
-	// set WP_CONTENT_URL
-	if ( ! defined( 'WP_CONTENT_URL' ) ) define( 'WP_CONTENT_URL', site_url( 'wp-content') );
+// set WP_CONTENT_URL
+if ( ! defined( 'WP_CONTENT_URL' ) ) define( 'WP_CONTENT_URL', site_url( 'wp-content') );
 	
-	// get fb app id and secret and ttl
-	$shariff3UU_advanced = (array) get_option( 'shariff3UU_advanced' );
+// get fb app id and secret and ttl
+$shariff3UU_advanced = (array) get_option( 'shariff3UU_advanced' );
 
-	// set fb api and secret
-	if ( isset( $shariff3UU_advanced['fb_id'] ) && isset( $shariff3UU_advanced['fb_secret'] ) ) {
-			$tmp["Facebook"]["app_id"] = absint( $shariff3UU_advanced['fb_id'] );
-			$tmp["Facebook"]["secret"] = sanitize_text_field( $shariff3UU_advanced['fb_secret'] );
-		}
+// set fb api and secret
+if ( isset( $shariff3UU_advanced['fb_id'] ) && isset( $shariff3UU_advanced['fb_secret'] ) ) {
+	$tmp["Facebook"]["app_id"] = absint( $shariff3UU_advanced['fb_id'] );
+	$tmp["Facebook"]["secret"] = sanitize_text_field( $shariff3UU_advanced['fb_secret'] );
+}
 	
-	// if we have a constant for the ttl (default is 60 seconds)
-	if ( defined( 'SHARIFF_BACKEND_TTL' ) ) $tmp["cache"]["ttl"] = SHARIFF_BACKEND_TTL;
-	// elseif check for option from the WordPress plugin, must be between 60 and 7200 seconds
-	else {
-		if ( isset( $shariff3UU_advanced['ttl'] ) ) {
-			$ttl = absint( $shariff3UU_advanced['ttl'] );
-			if ( $ttl < '61' ) $ttl = '60';
-			elseif ( $ttl > '7200' ) $ttl = '7200';
-			else $tmp["cache"]["ttl"] = $ttl;
-		}
+// if we have a constant for the ttl (default is 60 seconds)
+if ( defined( 'SHARIFF_BACKEND_TTL' ) ) $tmp["cache"]["ttl"] = SHARIFF_BACKEND_TTL;
+// elseif check for option from the WordPress plugin, must be between 60 and 7200 seconds
+else {
+	if ( isset( $shariff3UU_advanced['ttl'] ) ) {
+		$ttl = absint( $shariff3UU_advanced['ttl'] );
+		if ( $ttl < '61' ) $ttl = '60';
+		elseif ( $ttl > '7200' ) $ttl = '7200';
+		else $tmp["cache"]["ttl"] = $ttl;
 	}
-
-	// if we have a constant for the tmp-dir
-	if ( defined( 'SHARIFF_BACKEND_TMPDIR' ) ) $tmp["cache"]["cacheDir"] = SHARIFF_BACKEND_TMPDIR;
-	
-	// if we do not have a tmp-dir, we use the content dir of WP
-	if ( empty( $tmp["cache"]["cacheDir"] ) ) {
-		$upload_dir = wp_upload_dir('/');
-		$cache_dir = $upload_dir['basedir'] . '/shariff3uu_cache';
-		// if it doesn't exit, try to create it
-		if( ! file_exists( $cache_dir ) ) {
-			wp_mkdir_p( $cache_dir );
-		}
-		$tmp["cache"]["cacheDir"] = $cache_dir;
-	}
-
-	// final check that temp dir is usuable
-	if ( ! is_writable( $tmp["cache"]["cacheDir"] ) ) die( "No usable tmp dir found. Please check " . $tmp["cache"]["cacheDir"] );
-
-	// use proxy if set in wp_config.php
-	if ( defined( 'WP_PROXY_HOST' ) && defined( 'WP_PROXY_PORT' ) ) {
-    	$proxy = WP_PROXY_HOST . ':' . WP_PROXY_PORT;
-    	$tmp["client"]["proxy"] = $proxy;
-	}
-
-	// start backend
-	$shariff = new Backend( $tmp );
-
-	// draw results, if we have some
-	$share_counts = $shariff->get( $_GET["url"] );
-	if ( isset( $share_counts ) && $share_counts != null ) {
-		echo json_encode( $share_counts );
-	}
-	else {
-		// it's actually just a guess, but very likely
-		echo 'Invalid URL (e.g. wrong domain)! ';
-		// in case we have some usefull information, but most likley will be "null"
-		echo 'Message: ' . json_encode( $share_counts );
-	}
-  }
 }
 
-Application::run();
+// if we have a constant for the tmp-dir
+if ( defined( 'SHARIFF_BACKEND_TMPDIR' ) ) $tmp["cache"]["cacheDir"] = SHARIFF_BACKEND_TMPDIR;
+	
+// if we do not have a tmp-dir, we use the content dir of WP
+if ( empty( $tmp["cache"]["cacheDir"] ) ) {
+	$upload_dir = wp_upload_dir('/');
+	$cache_dir = $upload_dir['basedir'] . '/shariff3uu_cache';
+	// if it doesn't exit, try to create it
+	if( ! file_exists( $cache_dir ) ) {
+		wp_mkdir_p( $cache_dir );
+	}
+	$tmp["cache"]["cacheDir"] = $cache_dir;
+}
+
+// final check that temp dir is usuable
+if ( ! is_writable( $tmp["cache"]["cacheDir"] ) ) die( "No usable tmp dir found. Please check " . $tmp["cache"]["cacheDir"] );
+
+// use proxy if set in wp_config.php
+if ( defined( 'WP_PROXY_HOST' ) && defined( 'WP_PROXY_PORT' ) ) {
+   	$proxy = WP_PROXY_HOST . ':' . WP_PROXY_PORT;
+   	$tmp["client"]["proxy"] = $proxy;
+}
+
+// start backend
+$shariff = new Backend( $tmp );
+
+// draw results, if we have some
+$share_counts = $shariff->get( $_GET["url"] );
+if ( isset( $share_counts ) && $share_counts != null ) {
+	echo json_encode( $share_counts );
+}
+else {
+	// it's actually just a guess, but very likely
+	echo 'Invalid URL (e.g. wrong domain)! ';
+	// in case we have some usefull information, but most likley will be "null"
+	echo 'Message: ' . json_encode( $share_counts );
+}
+?>
--- shariff/readme.txt
+++ shariff/readme.txt
@@ -153,6 +153,7 @@ A: Yes, take a look at the Mail Form tab on the plugin options page.
 = 2.4.3 =
 - fix proxy settings
 - fix PHP error notice caused by a race condition around concurrent requests in Zend_Cache
+- fix PHP notice and error in backend on multisite
 
 = 2.4.2 =
 - fix lang attribute again
