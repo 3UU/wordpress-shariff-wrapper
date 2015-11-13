e3caaf0023382c706b13741a260d7712aa7a4ddf
startef <not@posteo.de>
2015-11-13 23:14:52 +0100

    fix-facebookid
    Da muss man einmal drauf kommen. Die Facebook ID besteht nur aus
    Zahlen. Also warum nicht einmal checken, ob das auch wirklich der Fall
    ist. Sicher ist sicher und so. Super Idee. Da wundert man sich, warum
    es bei einem selbst funktioniert, bei der großen Masse auch, aber bei
    so ein paar Leute, da geht es irgendwie nicht und Facebook meldet so
    einen „Es ist was passiert.“-Fehler. Bis man darauf kommt, dass man
    beim check ja logischerweise das Ganze zu nem Integer macht. Das ist
    auch nicht schlimm, es sei denn man befindet sich noch auf einem 32-bit
    gammel Server, dann ist bei 2 Billionen Schluss und damit passt die
    ganze schöne ID da leider nicht rein.

--- shariff/shariff.php
+++ shariff/shariff.php
@@ -3,7 +3,7 @@
  * Plugin Name: Shariff Wrapper
  * Plugin URI: http://www.3uu.org/plugins.htm
  * Description: This is a wrapper to Shariff. It enables shares with Twitter, Facebook ... on posts, pages and themes with no harm for visitors privacy.
- * Version: 3.1.1
+ * Version: 3.1.2
  * Author: 3UU, JP
  * Author URI: http://www.DatenVerwurstungsZentrale.com/
  * License: http://opensource.org/licenses/MIT
@@ -103,7 +103,7 @@ else {
 function shariff3UU_update() {
 
 	/******************** ADJUST VERSION ********************/
-	$code_version = "3.1.1"; // set code version - needs to be adjusted for every new version!
+	$code_version = "3.1.2"; // set code version - needs to be adjusted for every new version!
 	/******************** ADJUST VERSION ********************/
 
 	// do we want to display an admin notice after the update?
