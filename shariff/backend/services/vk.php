a75ca920c5279a22033b5657c249468adbb3ce53
3UU <support@3uu.net>
2015-11-14 15:25:37 +0000

    VK drin. Erstmal nur eingebaut und nicht in readme oder der shariff.php dokumentiert.

---
+++ shariff/backend/services/vk.php
@@ -0,0 +1,23 @@
+<?php
+
+// Twitter
+
+// fetch counts
+$vk = sanitize_text_field( wp_remote_retrieve_body( wp_remote_get( 'http://vk.com/share.php?act=count&index=1&url=' . $post_url ) ) );
+if (!$vk){
+	$share_counts['errors']['vk'] = "VK Error! Message: " . $vk;
+}else{
+	preg_match('/^VK.Share.count\((\d+),\s+(\d+)\);$/i', $vk, $matches);
+	$vk_count = $matches[1];	
+}
+
+// store results, if we have some
+if ( isset( $vk_count ) ) {
+	$share_counts['vk'] = $vk_count;
+}
+// otherwise store the error message
+else {
+	$share_counts['errors']['vk'] = "VK Error! Message: " . $vk;
+}
+
+?>
