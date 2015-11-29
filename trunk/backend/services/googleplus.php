2072128bfa1bcdd801cc464b4add44d79bef3ee1
3UU <3UU@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-29 17:11:17 +0000

    fix ttl setting on statistic tab; reduce timeout for post requests to five seconds
    git-svn-id: https://plugins.svn.wordpress.org/shariff@1296868 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/backend/services/googleplus.php
+++ trunk/backend/services/googleplus.php
@@ -21,7 +21,7 @@ $google_options = array(
 // set post options
 $google_post_options = array(
 	'method' => 'POST',
-	'timeout' => 45,
+	'timeout' => 5,
 	'redirection' => 5,
 	'httpversion' => '1.0',
 	'blocking' => true,
@@ -30,16 +30,12 @@ $google_post_options = array(
 );
 
 // fetch counts
-$google = sanitize_text_field( wp_remote_retrieve_body( wp_remote_post( 'https://clients6.google.com/rpc?key=AIzaSyCKSbrvQasunBoV16zDH9R33D88CeLr9gQ', $google_post_options ) ) );
-$google_json = json_decode( $google, true );
+$googleplus = sanitize_text_field( wp_remote_retrieve_body( wp_remote_post( 'https://clients6.google.com/rpc?key=AIzaSyCKSbrvQasunBoV16zDH9R33D88CeLr9gQ', $google_post_options ) ) );
+$google_json = json_decode( $googleplus, true );
 
 // store results, if we have some
 if ( isset( $google_json['result']['metadata']['globalCounts']['count'] ) ) {
-	$share_counts['googleplus'] = $google_json['result']['metadata']['globalCounts']['count'];
-}
-// otherwise store the error message
-else {
-	$share_counts['errors']['google'] = "GooglePlus Error! Message: " . $google;
+	$share_counts['googleplus'] = intval( $google_json['result']['metadata']['globalCounts']['count'] );
 }
 
 ?>
