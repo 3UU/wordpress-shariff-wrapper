5e333bc734857d98ae25fc7071fb2daa35c444de
starguide <starguide@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-06-15 18:54:06 +0000

    Rename 1.0
    git-svn-id: https://plugins.svn.wordpress.org/shariff@1437300 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/Stable1.0/backend/src/Backend/ServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Heise\Shariff\Backend;
-
-interface ServiceInterface
-{
-    public function getRequest($url);
-    public function extractCount($data);
-    public function getName();
-}
