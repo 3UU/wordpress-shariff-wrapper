97f75fe2bde5d1d330ee269abc4af7d4dac1b56f
starguide <starguide@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-09-21 15:09:55 +0000

    Release 4.3
    git-svn-id: https://plugins.svn.wordpress.org/shariff@1499744 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/4.3.0/admin/admin_metabox.php
+++ tags/4.3.0/admin/admin_metabox.php
@@ -39,7 +39,23 @@ function shariff3uu_build_metabox() {
 	echo '<input type="checkbox" name="shariff_metabox_disable"';
 	if ( isset( $shariff_metabox_disable ) ) echo checked( $shariff_metabox_disable, 1, 0 );
 	echo '>';
-	echo '<label for="shariff_metabox_disable">' . __( 'Disable Shariff on this post or page.', 'shariff' ) . '</label></p>';
+	echo '<label for="shariff_metabox_disable">' . __( 'Disable Shariff for this content.', 'shariff' ) . '</label></p>';
+	
+	// retrieve the current metabox add before and after values
+	$shariff_metabox_before = get_post_meta( get_the_ID(), 'shariff_metabox_before', true );
+	$shariff_metabox_after = get_post_meta( get_the_ID(), 'shariff_metabox_after', true );
+	// add Shariff checkboxes
+	echo '<p><strong>' . __( 'Add Shariff', 'shariff' ) . '</strong><br>';
+	// before checkbox
+	echo '<input type="checkbox" name="shariff_metabox_before"';
+	if ( isset( $shariff_metabox_before ) ) echo checked( $shariff_metabox_before, 1, 0 );
+	echo '>';
+	echo '<label for="shariff_metabox_before">' . __( 'Add buttons <u>before</u> this content.', 'shariff' ) . '</label><br>';
+	// after checkbox
+	echo '<input type="checkbox" name="shariff_metabox_after"';
+	if ( isset( $shariff_metabox_after ) ) echo checked( $shariff_metabox_after, 1, 0 );
+	echo '>';
+	echo '<label for="shariff_metabox_after">' . __( 'Add buttons <u>after</u> this content.', 'shariff' ) . '</label></p>';
 	
 	// retrieve the current metabox media value (pinterest image)
 	$shariff_metabox_media = get_post_meta( get_the_ID(), 'shariff_metabox_media', true );
@@ -60,43 +76,67 @@ function shariff3uu_build_metabox() {
 	echo '<input type="checkbox" name="shariff_metabox_ignore_widget"';
 	if ( isset( $shariff_metabox_ignore_widget ) ) echo checked( $shariff_metabox_ignore_widget, 1, 0 );
 	echo '>';
-	echo '<label for="shariff_metabox_ignore_widget">' . __( 'Do not affect widgets.', 'shariff' ) . '</label></p>';
+	echo '<label for="shariff_metabox_ignore_widget">' . __( 'Do not affect buttons in widgets.', 'shariff' ) . '</label></p>';
 }
 
 // save meta data
-function shariff3UU_save_metabox_data( $post_id ) {
+function shariff3UU_save_metabox_data( $post_id, $post ) {
 	// check nonce and if shariff_metabox is set
 	if ( isset( $_REQUEST['shariff_metabox_nonce'] ) && wp_verify_nonce( $_REQUEST['shariff_metabox_nonce'], basename( __FILE__ ) ) ) {
-		// save meta box disable
-		if ( isset( $_POST['shariff_metabox_disable'] ) && $_POST['shariff_metabox_disable'] == 'on' ) {
-			update_post_meta( $post_id, 'shariff_metabox_disable', '1' );
+		// check if we are not autosaving or previewing (revision)
+		$post_type_object = get_post_type_object( $post->post_type );
+		if ( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) || wp_is_post_revision( $post ) || ( is_multisite() && ms_is_switched() ) || ! current_user_can( $post_type_object->cap->edit_post, $post_id ) ) {
+			return;
 		}
+		// else we are good to go and can save our meta box data
 		else {
-			delete_post_meta( $post_id, 'shariff_metabox_disable', '1' );
-		}
-		
-		// save meta box media
-		if ( isset( $_POST['shariff_metabox_media'] ) && ! empty( $_POST['shariff_metabox_media'] ) ) {
-			update_post_meta( $post_id, 'shariff_metabox_media', esc_url_raw( $_POST['shariff_metabox_media'] ) );
-		}
-		else {
-			delete_post_meta( $post_id, 'shariff_metabox_media' );
-		}
-		
-		// save meta box shortcode
-		if ( isset( $_POST['shariff_metabox'] ) && ! empty( $_POST['shariff_metabox'] ) ) {
-			update_post_meta( $post_id, 'shariff_metabox', wp_kses( $_POST['shariff_metabox'], $GLOBALS["allowed_tags"] ) );
-		}
-		else {
-			delete_post_meta( $post_id, 'shariff_metabox' );
-		}
+			// save meta box disable
+			if ( isset( $_POST['shariff_metabox_disable'] ) && $_POST['shariff_metabox_disable'] == 'on' ) {
+				update_post_meta( $post_id, 'shariff_metabox_disable', '1' );
+			}
+			else {
+				delete_post_meta( $post_id, 'shariff_metabox_disable', '1' );
+			}
+			
+			// save meta box add before value
+			if ( isset( $_POST['shariff_metabox_before'] ) && $_POST['shariff_metabox_before'] == 'on' ) {
+				update_post_meta( $post_id, 'shariff_metabox_before', '1' );
+			}
+			else {
+				delete_post_meta( $post_id, 'shariff_metabox_before', '1' );
+			}
+			
+			// save meta box add after value
+			if ( isset( $_POST['shariff_metabox_after'] ) && $_POST['shariff_metabox_after'] == 'on' ) {
+				update_post_meta( $post_id, 'shariff_metabox_after', '1' );
+			}
+			else {
+				delete_post_meta( $post_id, 'shariff_metabox_after', '1' );
+			}
+			
+			// save meta box media
+			if ( isset( $_POST['shariff_metabox_media'] ) && ! empty( $_POST['shariff_metabox_media'] ) ) {
+				update_post_meta( $post_id, 'shariff_metabox_media', esc_url_raw( $_POST['shariff_metabox_media'] ) );
+			}
+			else {
+				delete_post_meta( $post_id, 'shariff_metabox_media' );
+			}
+			
+			// save meta box shortcode
+			if ( isset( $_POST['shariff_metabox'] ) && ! empty( $_POST['shariff_metabox'] ) ) {
+				update_post_meta( $post_id, 'shariff_metabox', wp_kses( $_POST['shariff_metabox'], $GLOBALS["allowed_tags"] ) );
+			}
+			else {
+				delete_post_meta( $post_id, 'shariff_metabox' );
+			}
 
-		// save meta box ignore widgets
-		if ( isset( $_POST['shariff_metabox_ignore_widget'] ) && $_POST['shariff_metabox_ignore_widget'] == 'on' ) {
-			update_post_meta( $post_id, 'shariff_metabox_ignore_widget', '1' );
-		}
-		else {
-			delete_post_meta( $post_id, 'shariff_metabox_ignore_widget', '1' );
+			// save meta box ignore widgets
+			if ( isset( $_POST['shariff_metabox_ignore_widget'] ) && $_POST['shariff_metabox_ignore_widget'] == 'on' ) {
+				update_post_meta( $post_id, 'shariff_metabox_ignore_widget', '1' );
+			}
+			else {
+				delete_post_meta( $post_id, 'shariff_metabox_ignore_widget', '1' );
+			}
 		}
 	}
 }
