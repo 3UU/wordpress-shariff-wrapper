5edf1363e0ba5eb77907bdfeded746c2f78396d5
Ensam57 <116890575+Ensam57@users.noreply.github.com>
2024-02-21 16:21:58 +0100

    Update shariff-buffer.php
    Add button text label.

--- shariff/services/shariff-buffer.php
+++ shariff/services/shariff-buffer.php
@@ -29,6 +29,12 @@ if ( isset( $frontend ) && 1 === $frontend ) {
 	// Backend available?
 	$backend_available = 1;
 
+	// Button text label.
+	$button_text_array = array(
+		'en' => 'Buffer',
+		'fr' => 'Buffer',
+	);
+
 	// Button alt label.
 	$button_title_array = array(
 		'bg' => 'Сподели в buffer',
