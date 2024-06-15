9c24c1264a94f2d6ca56902355d2a063fe7f5eec
3UU <3UU@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2024-06-15 17:03:05 +0000

    removed Reddit from the share counts; fix to avoid unauthenticated local file inclusion (with many thanks to haidv35)
    git-svn-id: https://plugins.svn.wordpress.org/shariff@3103137 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/4.6.14/services/shariff-mewe.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Will be included in the shariff.php only, when MeWe is requested as a service.
+ *
+ * @package Shariff Wrapper
+ */
+
+// Prevent direct calls.
+if ( ! class_exists( 'WP' ) ) {
+	die();
+}
+
+// Check if we need the frontend or the backend part.
+if ( isset( $frontend ) && 1 === $frontend ) {
+	// Service URL.
+	$service_url = esc_url( 'https://mewe.com/share' );
+
+	// Build the button URL.
+	$button_url = $service_url . '?link=' . $share_url;
+
+	// Set the Colors (Hexadecimal including the #).
+	$main_color      = '#16387D';
+	$secondary_color = '#187ca5';
+	$wcag_color      = '#16387D';
+
+	// SVG icon.
+	$svg_icon = '<svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="' . $main_color . '" d="M7.48,9.21a1.46,1.46,0,1,1-2.92,0h0a1.46,1.46,0,1,1,2.92,0Zm7.3,0a1.52,1.52,0,1,1-1.52-1.52A1.52,1.52,0,0,1,14.78,9.21Zm8.52,0a1.52,1.52,0,1,1-1.52-1.52A1.52,1.52,0,0,1,23.3,9.21Zm8.53,0a1.47,1.47,0,0,1-2.93,0h0a1.47,1.47,0,0,1,2.93,0ZM.17,13.35a1.09,1.09,0,0,1,1.1-1.09h.12a1,1,0,0,1,1,.61L6,18.46l3.65-5.59a.8.8,0,0,1,.85-.61h.25a1,1,0,0,1,1,1.09v9.74a1,1,0,0,1-.7,1.19,1.24,1.24,0,0,1-.27,0,1.11,1.11,0,0,1-1.11-1.09s0-.09,0-.13V16.15l-2.8,4.26a1.08,1.08,0,0,1-1,.61c-.37,0-.61-.24-.86-.61l-2.8-4.26v7.06a1,1,0,0,1-1.09,1,1,1,0,0,1-1-1Zm13.15.37a1.35,1.35,0,0,1,0-.49,1,1,0,0,1,1-1,1.05,1.05,0,0,1,1,.73l2.8,8.15L20.75,13a.92.92,0,0,1,1-.73h.12c.61,0,1,.24,1.1.73l2.68,8.15L28.3,13a1,1,0,0,1,1-.73,1.06,1.06,0,0,1,1.09,1,1,1,0,0,1,0,.49l-3.65,9.74a1,1,0,0,1-1,.85H25.5a1.31,1.31,0,0,1-1.1-.85l-2.56-7.67-2.67,7.67a1.32,1.32,0,0,1-1.1.85h-.24a1,1,0,0,1-1-.85Z"/></svg>';
+
+	// Backend available?
+	$backend_available = 0;
+
+	// Button alt label.
+	$button_title_array = array(
+		'bg' => 'Сподели във MeWe',
+		'cs' => 'Sdílet na MeWeu',
+		'da' => 'Del på MeWe',
+		'de' => 'Bei MeWe teilen',
+		'en' => 'Share on MeWe',
+		'es' => 'Compartir en MeWe',
+		'fi' => 'Jaa MeWeissa',
+		'fr' => 'Envoyer par MeWe',
+		'hr' => 'Podijelite na MeWeu',
+		'hu' => 'Megosztás MeWeon',
+		'it' => 'Condividi su MeWe',
+		'ja' => 'フェイスブック上で共有',
+		'ko' => '페이스북에서 공유하기',
+		'nl' => 'Delen op MeWe',
+		'no' => 'Del på MeWe',
+		'pl' => 'Udostępnij na MeWeu',
+		'pt' => 'Compartilhar no MeWe',
+		'ro' => 'Partajează pe MeWe',
+		'ru' => 'Поделиться на MeWe',
+		'sk' => 'Zdieľať na MeWeu',
+		'sl' => 'Deli na MeWeu',
+		'sr' => 'Podeli na MeWe-u',
+		'sv' => 'Dela på MeWe',
+		'tr' => 'MeWe\'ta paylaş',
+	);
+}
---
+++ tags/4.6.14/services/shariff-pinterest.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Will be included in the shariff.php only, when Pinterest is requested as a service.
+ *
+ * @package Shariff Wrapper
+ */
+
+// Prevent direct calls.
+if ( ! class_exists( 'WP' ) ) {
+	die();
+}
+
+// Check if function catch_image exists.
+if ( ! function_exists( 'shariff3uu_catch_image' ) ) {
+	/**
+	 * Helper function to get the first image of a post.
+	 */
+	function shariff3uu_catch_image() {
+		$result = preg_match_all( '/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', get_post_field( 'post_content', get_the_ID() ), $matches );
+		if ( array_key_exists( 0, $matches[1] ) ) {
+			return $matches[1][0];
+		} else {
+			return '';
+		}
+	};
+};
+
+// Check if we need the frontend or the backend part.
+if ( isset( $frontend ) && 1 === $frontend ) {
+	// Service URL.
+	$service_url = esc_url( 'https://www.pinterest.com/pin/create/link/' );
+
+	// Get an image for pinterest (attribute -> featured image -> first image -> default image -> shariff hint).
+	if ( array_key_exists( 'services', $atts ) ) {
+		if ( strstr( $atts['services'], 'pinterest' ) ) {
+			if ( array_key_exists( 'media', $atts ) ) {
+				$share_media = esc_html( $atts['media'] );
+			} else {
+				$feat_image = wp_get_attachment_url( get_post_thumbnail_id() );
+				if ( ! empty( $feat_image ) ) {
+					$share_media = esc_html( $feat_image );
+				} else {
+					$first_image = shariff3uu_catch_image();
+					if ( ! empty( $first_image ) ) {
+						$share_media = esc_html( $first_image );
+					} else {
+						if ( isset( $shariff3uu['default_pinterest'] ) ) {
+							$share_media = $shariff3uu['default_pinterest'];
+						} else {
+							$share_media = plugins_url( '/images/defaultHint.png', dirname( __FILE__ ) );
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Build button URL.
+	$button_url = $service_url . '?url=' . $share_url . '&media=' . rawurlencode( $share_media ) . '&description=' . $share_title;
+
+	// Colors.
+	$main_color      = '#cb2027';
+	$secondary_color = '#e70f18';
+	$wcag_color      = '#AE1920';
+
+	// SVG icon.
+	$svg_icon = '<svg width="32px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 32"><path fill="' . $main_color . '" d="M27.4 16q0 3.7-1.8 6.9t-5 5-6.9 1.9q-2 0-3.9-0.6 1.1-1.7 1.4-2.9 0.2-0.6 1-3.8 0.4 0.7 1.3 1.2t2 0.5q2.1 0 3.8-1.2t2.7-3.4 0.9-4.8q0-2-1.1-3.8t-3.1-2.9-4.5-1.2q-1.9 0-3.5 0.5t-2.8 1.4-2 2-1.2 2.3-0.4 2.4q0 1.9 0.7 3.3t2.1 2q0.5 0.2 0.7-0.4 0-0.1 0.1-0.5t0.2-0.5q0.1-0.4-0.2-0.8-0.9-1.1-0.9-2.7 0-2.7 1.9-4.6t4.9-2q2.7 0 4.2 1.5t1.5 3.8q0 3-1.2 5.2t-3.1 2.1q-1.1 0-1.7-0.8t-0.4-1.9q0.1-0.6 0.5-1.7t0.5-1.8 0.2-1.4q0-0.9-0.5-1.5t-1.4-0.6q-1.1 0-1.9 1t-0.8 2.6q0 1.3 0.4 2.2l-1.8 7.5q-0.3 1.2-0.2 3.2-3.7-1.6-6-5t-2.3-7.6q0-3.7 1.9-6.9t5-5 6.9-1.9 6.9 1.9 5 5 1.8 6.9z"/></svg>';
+
+	// Backend available?
+	$backend_available = 1;
+
+	// Button text label.
+	$button_text_array = array(
+		'de' => 'merken',
+		'en' => 'save',
+		'es' => 'ahorrar',
+		'fr' => 'enregistrer',
+		'it' => 'salva',
+	);
+
+	// Button alt label.
+	$button_title_array = array(
+		'bg' => 'Сподели в Pinterest',
+		'cs' => 'Přidat na Pinterest',
+		'da' => 'Del på Pinterest',
+		'de' => 'Bei Pinterest pinnen',
+		'en' => 'Pin it on Pinterest',
+		'es' => 'Ahorrar en Pinterest',
+		'fi' => 'Jaa Pinterestissä',
+		'fr' => 'Envoyer par Pinterest',
+		'hr' => 'Podijelite na Pinterest',
+		'hu' => 'Megosztás Pinteresten',
+		'it' => 'Condividi su Pinterest',
+		'ja' => 'Pinterest上で共有',
+		'ko' => 'Pinterest에서 공유하기',
+		'nl' => 'Delen op Pinterest',
+		'no' => 'Del på Pinterest',
+		'pl' => 'Udostępnij przez Pinterest',
+		'pt' => 'Compartilhar no Pinterest',
+		'ro' => 'Partajează pe Pinterest',
+		'ru' => 'Поделиться на Pinterest',
+		'sk' => 'Zdieľať na Pinterest',
+		'sl' => 'Deli na Pinterest',
+		'sr' => 'Podeli na Pinterest-u',
+		'sv' => 'Dela på Pinterest',
+		'tr' => 'Pinterest\'ta paylaş',
+		'zh' => '分享至Pinterest',
+	);
+} elseif ( isset( $backend ) && 1 === $backend ) {
+	// Fetch counts.
+	$pinterest      = sanitize_text_field( wp_remote_retrieve_body( wp_remote_get( 'https://api.pinterest.com/v1/urls/count.json?callback=x&url=' . $post_url ) ) );
+	$pinterest_json = rtrim( substr( $pinterest, 2 ), ')' );
+	$pinterest_json = json_decode( $pinterest_json, true );
+
+	// Store results, if we have some and record errors, if enabled (e.g. request from the status tab).
+	if ( isset( $pinterest_json['count'] ) ) {
+		$share_counts['pinterest'] = intval( $pinterest_json['count'] );
+	} elseif ( isset( $record_errors ) && 1 === $record_errors ) {
+		$service_errors['pinterest'] = $pinterest;
+	}
+}
